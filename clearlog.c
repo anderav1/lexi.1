a8aed3cb29a72c1dbee74a36997d4b8bc66edec4
anderav1 <avabc5@mail.umsl.edu>
2021-09-10 13:16:17 -0500

    Implemented clearlog.c

--- clearlog.c
+++ clearlog.c
@@ -1,10 +1,20 @@
 #include "log.h"
 
+// log_t* headptr and log_t* tailptr defined in log.c
+
 // clearlog function
 // release all the storage that has been allocated for the logged msg and
 // empty the list of logged msgs
 void clearlog(void) {
-	// deallocate msg storage
+	log_t* currentnode = headptr;
+	log_t* nextnode;
 
-	// empty the list of logged msgs
+	// traverse the list, deleting each node & releasing memory
+	while (currentnode != NULL) {
+		nextnode = currentnode->next;
+		free(currentnode); // deallocate memory
+		currentnode = nextnode;
+	}
+	
+	headptr = NULL;
 }
