688df8a4b38b66284b6411ef2db7aee086725578
anderav1 <avabc5@mail.umsl.edu>
2021-09-12 12:20:53 -0500

    Added output file arg handling to driver.c

--- driver.c
+++ driver.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 
 // driver program
 int main(int argc, char* argv[]) {
 	char c;
 	int sec;
+
+	// check for optional args
 	while ((c = getopt(argc, argv, "ht:")) != -1) {
 		switch (c) {
 		case 'h':
@@ -18,4 +21,26 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 	}	
+	
+	// check for output filename arg
+	char* ofilename;
+	char* defaultfilename = "messages.log";
+	ofilename = (char*)malloc(strlen());
+	strcpy(ofilename, "message.log");
+	if (argv[optind] != NULL) {
+		ofilename = (char*)malloc(strlen(argv[optind]));
+		strcpy(ofilename, argv[optind]);
+	} else {
+		ofilename = (char*)malloc(strlen(defaultfilename));
+		strcpy(ofilename, defaultfilename);
+	}
+
+	// read the messages in from a txt file
+	
+
+
+
+
+
+
 }
